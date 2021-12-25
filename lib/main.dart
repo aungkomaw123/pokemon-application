8ab0e85eeb2703b04e5b0e5fafad2fbe82b7c1f8
aungkomaw123 <aungkomaw.123@gmail.com>
2021-12-25 10:11:49 +0630

    Pokemon Application

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:pokemon/home_screen.dart';
+
+void main() {
+  runApp(Pokemon());
+}
+
+class Pokemon extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
